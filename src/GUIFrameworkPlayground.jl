48412570db89b20a28a29f998612d27bc4dde871
Jonathan Bieler <JBieler@sophiagenetics.com>
2018-02-16 17:23:51 +0100

    initial commit

--- src/GUIFrameworkPlayground.jl
+++ src/GUIFrameworkPlayground.jl
@@ -1,5 +1,29 @@
 module GUIFrameworkPlayground
 
-# package code goes here
+    using SDL2, GLVisualize, Cairo, ColorTypes, Graphics
 
+    import Cairo: rectangle
+    import Graphics: width, height
+    
+    abstract AbstractGUIBackend 
+
+    window(b::AbstractGUIBackend) = error("This method must be implemented.")
+    init!(b::AbstractGUIBackend) = error("This method must be implemented.")
+
+    abstract AbstractGUIWindow 
+
+    include("backends/SDL2GUIBackend.jl")
+    
 end # module
+
+G = GUIFrameworkPlayground
+
+b = G.SDL2GUIBackend()
+win = G.window(b,800,600)
+
+G.rectangle(win,100,100,200,100)
+G.update!(win)
+
+sleep(1.0)
+
+G.destroy(win)
---
+++ src/backends/SDL2GUIBackend.jl
@@ -0,0 +1,51 @@
+struct SDL2GUIBackend <: AbstractGUIBackend
+    function SDL2GUIBackend()
+        SDL2.init()
+        new()
+    end
+end
+
+struct SDL2Window <: AbstractGUIWindow
+    win::SDL2.SDLWindow
+    surfaces::SDL2.CairoSDLSurface
+
+    function SDL2Window(w,h,name)
+        win = SDL2.SDLWindow(w,h,name)
+        surfaces = SDL2.CairoSDLSurface(w,h)
+        new(win,surfaces)
+    end
+end
+
+width(win::SDL2Window)  = win.surfaces.cairo_surface.width
+height(win::SDL2Window) = win.surfaces.cairo_surface.height
+
+window(b::SDL2GUIBackend,w,h,name) = SDL2Window(w,h,name)
+window(b::SDL2GUIBackend,w,h) =  window(b,w,h,"")
+
+destroy(win::SDL2Window) = SDL2.DestroyWindow(win.win.win)#nice
+
+function update!(win::SDL2Window)
+
+    w, h = width(win), height(win)
+
+    renderer = win.win.renderer
+    tex = SDL2.CreateTextureFromSurface(renderer,win.surfaces.SDL_surface)
+    SDL2.RenderCopy(renderer, tex, C_NULL, pointer_from_objref(SDL2.Rect(0,0,w,h)))
+    SDL2.RenderPresent(renderer)
+end
+
+function rectangle(win::SDL2Window,x,y,w,h,color=RGB(0.9,0.9,0.9))
+    cr = win.surfaces.cairo_context
+    sdl_surf = win.surfaces.SDL_surface
+
+    save(cr)
+    set_source_rgb(cr,color.r,color.g,color.b)
+    rectangle(cr,x,y,w,h) 
+    fill(cr)
+    restore(cr)
+end
+
+function clear!(win,color=RGB(1.0,1.0,1.0))
+    w, h = width(win), height(win)
+    rectangle(win,0,0,w,h,color)
+end
